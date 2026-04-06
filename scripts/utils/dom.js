511874387d5cb57314994cd3018193fafd2eabcf
Hieu Le Trung <warmdevofficial@gmail.com>
2026-04-06 15:20:47 +0700

    fix(utils): add missing exports for querySelector shortcuts

--- scripts/utils/dom.js
+++ scripts/utils/dom.js
@@ -1,2 +1,2 @@
-const $ = document.querySelector.bind(document);
-const $$ = document.querySelectorAll.bind(document);
+export const $ = document.querySelector.bind(document);
+export const $$ = document.querySelectorAll.bind(document);
