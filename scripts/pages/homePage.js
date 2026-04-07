318bcba954e3717d5a4f488bbb9e1fe5ecae4117
Hieu Le Trung <warmdevofficial@gmail.com>
2026-04-07 23:41:23 +0700

    feat(ui): handle text content of header button

---
+++ scripts/pages/homePage.js
@@ -0,0 +1,21 @@
+import { showToast } from "../ui/toast.js";
+import { getCurrentUser } from "../services/authService.js";
+import { $ } from "../utils/dom.js";
+import { handleLogout } from "../controllers/logoutController.js";
+
+const btnHeader = $(".btn-header");
+const toastData = sessionStorage.getItem("toast");
+
+if (toastData) {
+  const { type, title, message } = JSON.parse(toastData);
+  showToast(type, title, message);
+  sessionStorage.removeItem("toast");
+}
+
+const currentUser = getCurrentUser();
+
+if (currentUser) {
+  btnHeader.textContent = "Đăng xuất";
+
+  btnHeader.onclick = handleLogout;
+}
