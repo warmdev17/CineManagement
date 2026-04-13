b0153aa70c45c1552ea141e5d0350c4cdcb0e1f0
Hieu Le Trung <warmdevofficial@gmail.com>
2026-04-13 12:04:07 +0700

    feat(ui): dynamic render movie to show

--- scripts/pages/homePage.js
+++ scripts/pages/homePage.js
@@ -1,10 +1,12 @@
 import { showToast } from "../ui/toast.js";
 import { getCurrentUser } from "../services/authService.js";
+import { getAllMovies } from "../repositories/movieRepository.js";
 import { $ } from "../utils/dom.js";
 import { handleLogout } from "../controllers/logoutController.js";
 import { routes } from "../config/constants.js";
 
 const btnHeader = $(".btn-header");
+const movieGrid = $(".movies-grid");
 const toastData = sessionStorage.getItem("toast");
 
 if (toastData) {
@@ -14,6 +16,7 @@ if (toastData) {
 }
 
 const currentUser = getCurrentUser();
+const movies = getAllMovies();
 
 if (currentUser) {
   btnHeader.textContent = "Đăng xuất";
@@ -24,3 +27,41 @@ if (currentUser) {
     window.location.href = routes.login;
   };
 }
+
+const createMovieTemplate = (movie) => {
+  return `
+    <div class="movie-card">
+      <div class="movie-poster">
+        <img
+          src="${movie.posterUrl}"
+          alt="${movie.title}"
+        />
+      </div>
+      <div class="movie-info">
+        <h3 class="movie-title">${movie.title}</h3>
+        <div class="movie-meta">
+          <span><i class="fa-regular fa-clock"></i> ${movie.duration} phút</span>
+          <span class="movie-dot">&bull;</span>
+          <span class="movie-genre">${movie.genres}</span>
+        </div>
+        <button class="btn btn-book">Mua Vé</button>
+      </div>
+    </div>
+  `;
+};
+
+const moviesToShow = movies.slice(0, 4);
+const renderCardMovie = () => {
+  if (!movieGrid) return;
+
+  if (!movies || movies.length === 0) {
+    movieGrid.innerHTML = `<p class="no-data">Hiện không có phim nào khả dụng</p>`;
+    return;
+  }
+
+  const htmlContent = moviesToShow.map(createMovieTemplate).join("");
+
+  movieGrid.innerHTML = htmlContent;
+};
+
+renderCardMovie();
